24573f797903c0e410bdf52fa494d9316ef04704
veronicaaleonova <veronica.leonova@ucalgary.ca>
2025-01-18 22:57:46 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,15 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Veronica Leonova",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "veronica.leonova@ucalgary.ca",
+        phone: "(587) 717-0858",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
 }
+
 
 export default data;
